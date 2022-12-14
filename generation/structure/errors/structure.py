2dcde5578badc51c7adfdad4fba4701b927e8f36
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-15 03:37:10 +1100

    Add ret type annotations and fix imports

--- generation/structure/errors/structure.py
+++ generation/structure/errors/structure.py
@@ -1,19 +1,20 @@
-from typing import Type
-from generation.structure.builder import IBuilder
+from typing import Type, NoReturn
+from generation.biome import Biome
+from generation.structure.builder import Builder
 
 
 class DirectorDoesNotExist(NotImplementedError):
-    def __init__(self, builder: Type[IBuilder]):
+    def __init__(self, builder: Type[Builder]) -> NoReturn:
         self.builder = builder
         super().__init__(f'Director for {self.builder} does not exist.')
 
 
 class BiomeNotImplemented(NotImplementedError):
-    def __init__(self, biome: Biome):
+    def __init__(self, biome: Biome) -> NoReturn:
         self.biome = biome
         super().__init__(f'Biome {self.biome} is not implemented.')
 
 
 class BiomeDoesNotExist(NotImplementedError):
-    def __init__(self):
+    def __init__(self) -> NoReturn:
         super().__init__(f'Given biome does not exist.')
