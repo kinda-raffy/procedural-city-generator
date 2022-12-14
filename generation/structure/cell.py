c247aee4ab93bd143dee6220c2657845863b4150
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-15 03:39:36 +1100

    Add Cell skeleton

--- generation/structure/cell.py
+++ generation/structure/cell.py
@@ -1,18 +1,78 @@
-from utils.temp import Vec3
-from typing import Literal
+from mcpi.vec3 import Vec3
+from typing import *
+from enum import *
+from abc import *
+from dataclasses import *
+
+
+class Node(Protocol):
+    """Interface"""
+    ...
+
+
+@unique
+class CellType(StrEnum):
+    # Base cells.
+    EMPTY = auto()
+    REGULAR = auto()
+    # Special cells.
+    LEVEL_ACCESS = auto()
+    ENTRANCE = auto()
+    POOL = auto()
+
+
+class NeighbouringCells(TypedDict):
+    # Horizontal.
+    NORTH: Cell | None
+    SOUTH: Cell | None
+    EAST: Cell | None
+    WEST: Cell | None
+    # Vertical.
+    UP: Cell | None
+    DOWN: Cell | None
 
 
 class Cell:
-    def __init__(self, vec: Vec3, c_type: Literal["door", "pool", "stair", "empty"]):
-        self.__pos: Vec3 = vec
-        self.type = c_type
+    def __init__(
+            self,
+            center_pos: Vec3,
+            /, *,
+            cell_type: CellType,
+            neighbours: NeighbouringCells
+    ):
+        self.__pos: Final[Vec3] = center_pos
+        self.__type: Final[CellType] = cell_type
+
+        self.__neighbours: Final[NeighbouringCells] = neighbours
+        self.__connected_cells: Set[Cell] = set()
+
+    def add_connected_cell(self, other: Cell) -> NoReturn:
+        assert other not in self.__connected_cells, f'{other} is already in connected_cells'
+        assert any([other == neighbour
+                    for neighbour in self.__neighbours.values()]), f'{other} is not a neighbour of {self}'
+        self.__connected_cells.add(other)
+
+    def __len__(self):
+        return len(self.__connected_cells)
 
     def __repr__(self):
-        return f"Cell({str(self.__pos)}, {self.type})"
+        return f"Cell({str(self.__pos)}, {self.__type})"
 
     def __eq__(self, other):
-        return self.__pos == other.__pos and self.type == other.type
+        return self.__pos == other.__pos
 
     def __hash__(self):
-        # Don't allow for overlapping cells.
-        return hash(self.__pos)
+        # Don't allow overlapping cells.
+        return hash(self.__pos)  # Use default mcpi.vec3 hashing.
+
+    @property
+    def get_connected_cells(self) -> Set[Cell]:
+        return self.__connected_cells
+
+    @property
+    def get_cell_type(self) -> CellType:
+        return self.__type
+
+    @property
+    def get_neigbours(self) -> NeighbouringCells:
+        return self.__neighbours
