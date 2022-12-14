d62e00955654085054335da971ac6d56154a1605
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-15 03:43:53 +1100

    Add config file reading for MaterialPack Separate FurniturePack into interiors.py for clarity Rename IBuilder instances to Builder

--- generation/structure/env.py
+++ generation/structure/env.py
@@ -1,19 +1,21 @@
-from errors.structure import (
-    BiomeDoesNotExist,
-    BiomeNotImplemented
-)
+from generation.structure.errors.structure import BiomeNotImplemented
 from typing import (
-    ClassVar,
+    Dict, Any, Final,
     NotRequired,
     TypedDict,
     Type
 )
 from utils.block_extension import BlockExt as BlocEx
 from generation import connection as conn
-from builder import IBuilder
+from builder import (
+    Builder,
+    HouseBuilder,
+    ApartmentBuilder,
+    SkyscraperBuilder
+)
 from generation.biome import Biome
 from dataclasses import dataclass
-from utils.temp import Vec3
+from mcpi.vec3 import Vec3
 import random
 import tomllib
 
@@ -23,128 +25,71 @@ class MaterialPack(TypedDict):
     # Sunder identifiers.
     _name_: NotRequired[str]
     _description_: NotRequired[str]
-
     # Horizontal surfaces.
     foundation: BlocEx
     stone: BlocEx
     upstairs_floor: BlocEx
-
     # Vertical surfaces.
     walls: BlocEx
     pillars: BlocEx
     slab: BlocEx
-
     # Structural elements.
     stairs: BlocEx
     windows: BlocEx
     door: BlocEx
-
     # Decorative elements.
     fence: BlocEx
     fence_gate: BlocEx
-
     # Pool materials.
     pool_wall: BlocEx
     pool_container: BlocEx
     pool_liquid: BlocEx
-
     # Roof materials.
     sunroof: BlocEx
     roof_outer: BlocEx
     roof_corner: BlocEx
 
 
-class FurniturePack(TypedDict):
-    """
-    Global furniture pack for structural generation.
-    Scales with the size of a given cell.
-    Divided into 3 categories:
-        - `true`: The true 3-Dimensional center of a cell.
-        - `side-center`: The center of a cell's 2-Dimensional side.
-        - `corner`: The corner of a cell's 2-Dimensional side.
-    """
-    # Sunder identifiers.
-    _name_: NotRequired[str]
-    _variation_: NotRequired[int]
-    _description_: NotRequired[str]
-
-    # True cell center.
-    true_center_floor: BlocEx
-    true_center: BlocEx
-    true_center_ceil: BlocEx
-
-    # Relative center of a given cell's ceil-side.
-    center_ceil_front: BlocEx
-    center_ceil_back: BlocEx
-    center_ceil_right: BlocEx
-    center_ceil_left: BlocEx
-
-    # Relative center of a given cell's floor-side.
-    center_floor_front: BlocEx
-    center_floor_back: BlocEx
-    center_floor_right: BlocEx
-    center_floor_left: BlocEx
-
-    # Ceiling corners: Front.
-    corner_ceil_front_right: BlocEx
-    corner_ceil_front_left: BlocEx
-
-    # Ceiling corners: Back.
-    corner_ceil_back_right: BlocEx
-    corner_ceil_back_left: BlocEx
-
-    # Floor corners: Front.
-    corner_floor_front_right: BlocEx
-    corner_floor_front_left: BlocEx
-
-    # Floor corners: Back.
-    corner_floor_back_right: BlocEx
-    corner_floor_back_left: BlocEx
-
-
 @dataclass(frozen=True, kw_only=True)
 class Environment:
     biome: Biome
-    builder: Type[IBuilder]
-    material_pack: ClassVar[MaterialPack]
+    builder: Type[Builder]
 
-    def __new__(cls, biome: Biome, builder: Type[IBuilder]):
-        if biome not in Biome:
-            raise BiomeDoesNotExist(biome)
+    def __new__(cls, biome, builder):
+        if biome in (Biome.LAVA, Biome.JUNGLE):
+            # TODO: Implement these biomes.
+            raise BiomeNotImplemented(biome)
         return super().__new__(cls)
 
-    @classmethod
-    def _load_material_pack(cls) -> MaterialPack:
+    def get_material_pack(self) -> MaterialPack:
         """Load materials from TOML file."""
-        pass
+        biome: Final[str] = self.biome.name.lower()
+        structure_type: Final[str] = self.builder.__name__.removesuffix('Builder').lower()
 
-    def material_selector(self) -> MaterialPack:
-        match biome := self.biome:
-            case Biome.GRASSY:
-                return random.choice(grass_biome)
-            case Biome.DESERT:
-                return random.choice(sand_biome)
-            case Biome.WATER:
-                return random.choice(water_biome)
-            case Biome.LAVA:
-                raise BiomeNotImplemented(biome)
-            case Biome.JUNGLE:
-                raise BiomeNotImplemented(biome)
-            case _:
-                raise BiomeDoesNotExist()
+        with open(f'config/material_packs/{biome}.toml', 'rb') as file:
+            config: Dict[str, Any] = tomllib.load(file)
+        assert structure_type in config['meta']['supported_structures'], f'{biome} does not support {structure_type}.'
+
+        pack: Dict[str, Any] = random.choice(config[biome][structure_type])
+        # Transform materials into BlockExt objects.
+        loaded_pack = {k: BlocEx[v.upper()] if isinstance(v, str) else v for k, v in pack.items()}
+        # Add metadata for logging.
+        for k, v in loaded_pack.pop('_info').items():
+            loaded_pack[f'_{k}_'] = v
+
+        return loaded_pack
 
     @staticmethod
     def clear_block(
             center: Vec3,
-            *,
+            /, *,
             radius: int = 7,
-            floor: int = 2
+            cell_floor: int = 2
     ) -> None:
         # Vec3 should be center of the building.
-        center.y += floor
+        center.y += cell_floor
         conn.setBlocks(
             center.x - radius, center.y, center.z - radius,     # Start coordinates.
             center + radius,                                    # End coordinates.
             BlocEx.AIR
         )
-
