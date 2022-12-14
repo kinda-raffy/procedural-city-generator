b5bbb55a8de0772b5abdb413a328bc1aaf25c1d4
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-15 03:45:57 +1100

    Create a DummyConnection for testing without server

--- generation/__init__.py
+++ generation/__init__.py
@@ -1,6 +1,21 @@
-from mcpi.minecraft import Minecraft
+from typing import NoReturn
+from mcpi.vec3 import Vec3
+
+
+class DummyConnection:
+    def set_block(self, *args) -> NoReturn: ...
+    def set_blocks(self, *args) -> NoReturn: ...
+    def get_block(self, *args) -> int: ...
+
 
 try:
+    from mcpi.minecraft import Minecraft
+
     connection: Minecraft = Minecraft.create()
 except ConnectionRefusedError:
-    print("Failed to connect to Minecraft")
+    print("Failed to connect to Minecraft. Using dummy connection.")
+
+    class Minecraft(DummyConnection):
+        pass
+
+    connection: Minecraft = Minecraft()
