7eb8819cc87b59ff718562ca4e9182b7b80076f5
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-15 03:40:41 +1100

    Remove 'I' naming for interfaces Optimise imports.

--- generation/structure/director.py
+++ generation/structure/director.py
@@ -2,31 +2,35 @@ from errors.structure import DirectorDoesNotExist
 from utils.temp import Vec3
 from builder import *
 from typing import *
-from typing_extensions import Self
 from abc import *
 
 
-class IDirector(metaclass=ABCMeta):
-    """Director Interface"""
-    # Bruh idk i was told this was a good idea.
+class Director(metaclass=ABCMeta):
+    """Interface"""
+    # TODO ~ Add Documentation.
 
-    def build(self) -> None: ...
+    @abstractmethod
+    def build(self) -> NoReturn: ...
 
 
-class ResidentialDirector(IDirector):
+@final
+class ResidentialDirector(Director):
     def __init__(
             self,
-            builder: Type[IResidentialBuilder],
+            builder: Type[ResidentialBuilder],
+            biome: Biome,
+            /, *,
             center: Vec3,
             door: Vec3,
-            biome: Biome
-    ) -> None:
-        self.__builder = builder
-        self.__center = center
-        self.__door = door
-        self.__biome = biome
+    ) -> NoReturn:
+        self.__builder: Final[
+            Type[ResidentialBuilder]
+        ] = builder
+        self.__biome:   Final[Biome] = biome
+        self.__center:  Final[Vec3] = center
+        self.__door:    Final[Vec3] = door
 
-    def build(self) -> None:
+    def build(self) -> NoReturn:
         with self.__builder(Biome()) as builder:
             builder.create_structure()
             builder.create_stairs()
@@ -36,34 +40,36 @@ class ResidentialDirector(IDirector):
             builder.create_windows()
 
 
+@final
 class DirectorFactory:
-    """
-    Director factory that determines an appropriate director at runtime.
-    """
+    """Director factory that determines an appropriate director at runtime."""
+    # TODO ~ Add Documentation.
+    D = TypeVar('Director', bound=Director)
+
     @staticmethod
     def register(
-            builder: Type[IBuilder],
+            builder: Type[Builder],
             center: Vec3,
             door: Vec3,
             biome: Biome
-    ) -> Type[IDirector]:
+    ) -> Type[D]:
         """
         Factory control switch. Registers a compatible director for a given builder.
         :return: An instantiated concrete director class.
         """
         # Grab inheritance tree.
         parent_tree = [cls.__name__ for cls in builder.__mro__]
-        if "IResidentialBuilder" in parent_tree:
+        if "ResidentialBuilder" in parent_tree:
             return ResidentialDirector(builder, center, door, biome)
-        elif "ICommercialBuilder" in parent_tree:
+        elif "CommercialBuilder" in parent_tree:
             raise NotImplementedError
-        elif "IParkBuilder" in parent_tree:
+        elif "ParkBuilder" in parent_tree:
             raise NotImplementedError
         else:
             raise DirectorDoesNotExist(builder)
 
 
-def debug() -> None:
+def debug() -> NoReturn:
     house = DirectorFactory.register(HouseBuilder, Vec3(), Vec3(), Biome())
     house.build()
 
