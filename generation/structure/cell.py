308fef44bedc5628e0aa34f62a3b26c64aadea18
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-19 05:24:05 +1100

    Add blueprint helper logic, expand CellType into specific types, add CellDirection and prettify type hints.

--- generation/structure/cell.py
+++ generation/structure/cell.py
@@ -1,78 +1,119 @@
+# FIXME ~ Narrow
+from __future__ import annotations
 from mcpi.vec3 import Vec3
 from typing import *
 from enum import *
 from abc import *
 from dataclasses import *
 
-
-class Node(Protocol):
-    """Interface"""
-    ...
+__all__ = [
+    'CellType',
+    'CellDirection',
+    'NeighbouringCells',
+    'Cell',
+]
 
 
 @unique
 class CellType(StrEnum):
-    # Base cells.
-    EMPTY = auto()
+    DETACHED = auto()  # Not part of structure.
+    # Cells that are part of the visible structure.
+    STRUCTURE_ENTRY = auto()
+    LEVEL_ENTRY = auto()
+    ABOVE_LEVEL_ENTRY = auto()
     REGULAR = auto()
-    # Special cells.
-    LEVEL_ACCESS = auto()
-    ENTRANCE = auto()
+    EMPTY = auto()
     POOL = auto()
 
 
+@unique
+class CellDirection(StrEnum):
+    NORTH = 'NORTH'
+    SOUTH = 'SOUTH'
+    EAST = 'EAST'
+    WEST = 'WEST'
+    UP = 'UP'
+    DOWN = 'DOWN'
+
+
 class NeighbouringCells(TypedDict):
-    # Horizontal.
-    NORTH: Cell | None
-    SOUTH: Cell | None
-    EAST: Cell | None
-    WEST: Cell | None
-    # Vertical.
-    UP: Cell | None
-    DOWN: Cell | None
+    """Contains references to neighbouring cells."""
+    # X-Axis.
+    NORTH: Optional[Cell]
+    SOUTH: Optional[Cell]
+    # Z-Axis.
+    EAST: Optional[Cell]
+    WEST: Optional[Cell]
+    # Y-Axis.
+    UP: Optional[Cell]
+    DOWN: Optional[Cell]
 
 
 class Cell:
+
     def __init__(
             self,
             center_pos: Vec3,
-            /, *,
+            /,
             cell_type: CellType,
-            neighbours: NeighbouringCells
+            *,
+            neighbours: Optional[NeighbouringCells] = None
     ):
-        self.__pos: Final[Vec3] = center_pos
-        self.__type: Final[CellType] = cell_type
+        self.pos: Final = center_pos
+        self._type: CellType = cell_type
+        self._neighbours: NeighbouringCells = neighbours \
+            if neighbours else dict.fromkeys(NeighbouringCells)
+        # Cells that coalesce to form a larger cell.
+        self._merged_cells: Set[Cell] = set()
 
-        self.__neighbours: Final[NeighbouringCells] = neighbours
-        self.__connected_cells: Set[Cell] = set()
+    def add_merged_cell(self, other: Self) -> NoReturn:
+        assert other not in self._merged_cells, \
+            f'{other!r} is already in connected_cells'
+        assert any([other == neighbour for neighbour in self._neighbours.values()]), \
+            f'{other!r} is not a neighbour of {self!r}'
+        self._merged_cells.add(other)
 
-    def add_connected_cell(self, other: Cell) -> NoReturn:
-        assert other not in self.__connected_cells, f'{other} is already in connected_cells'
-        assert any([other == neighbour
-                    for neighbour in self.__neighbours.values()]), f'{other} is not a neighbour of {self}'
-        self.__connected_cells.add(other)
+    def faces_environment(self) -> bool:
+        return any(
+            [neighbour is None or neighbour._type == CellType.DETACHED
+                for neighbour in self._neighbours.values()]
+        )
+
+    def faces_environment_direction(self) -> tuple:
+        return tuple(
+            [direction for direction, neighbour in self._neighbours.items()
+             if neighbour is None or neighbour._type == CellType.DETACHED]
+        )
 
     def __len__(self):
-        return len(self.__connected_cells)
+        return len(self._merged_cells)
 
     def __repr__(self):
-        return f"Cell({str(self.__pos)}, {self.__type})"
+        return f"Cell({str(self.pos)}, {self._type}, {len(self)=})"
 
     def __eq__(self, other):
-        return self.__pos == other.__pos
+        return self.pos == other.pos
 
     def __hash__(self):
         # Don't allow overlapping cells.
-        return hash(self.__pos)  # Use default mcpi.vec3 hashing.
+        return hash(self.pos)  # Use default mcpi.vec3 hashing.
 
     @property
-    def get_connected_cells(self) -> Set[Cell]:
-        return self.__connected_cells
+    def connected_cells(self) -> Set[Cell]:
+        return self._merged_cells
 
     @property
-    def get_cell_type(self) -> CellType:
-        return self.__type
+    def type_(self) -> CellType:
+        return self._type
+
+    @type_.setter
+    def type_(self, new_type: CellType) -> NoReturn:
+        self._type = new_type
 
     @property
-    def get_neigbours(self) -> NeighbouringCells:
-        return self.__neighbours
+    def neighbours(self) -> NeighbouringCells:
+        return self._neighbours
+
+    @neighbours.setter
+    def neighbours(self, neighbours: NeighbouringCells) -> NoReturn:
+        self._neighbours = neighbours
