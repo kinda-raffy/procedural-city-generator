95ef188b6c3f0e275b4efe25500e9dd42419830c
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-20 02:37:51 +1100

    Implement server connection proxy

--- generation/__init__.py
+++ generation/__init__.py
@@ -1,21 +1,28 @@
-from typing import NoReturn
-from mcpi.vec3 import Vec3
+from mcpi.minecraft import Minecraft
+from typing import NoReturn, Final
+import logging
+import inspect
 
 
-class DummyConnection:
-    def set_block(self, *args) -> NoReturn: ...
-    def set_blocks(self, *args) -> NoReturn: ...
-    def get_block(self, *args) -> int: ...
+class DummyConnection(Minecraft):
+    def postToChat(self, msg) -> NoReturn: ...
+    def setBlock(self, *args) -> NoReturn: ...
+    def setBlocks(self, *args) -> NoReturn: ...
+    def getBlock(self, *args) -> int: ...
+
+
+class ProxiedConnection(Minecraft):
+    def postToChat(self, msg: str) -> NoReturn:
+        caller: Final[int] = 1
+        module = inspect.getmodule(inspect.stack()[caller][0])
+        logging.info(f'BROADCAST by {module} : {msg}')
+        super(ProxiedConnection, self).postToChat(msg)
 
 
 try:
-    from mcpi.minecraft import Minecraft
-
-    connection: Minecraft = Minecraft.create()
+    connection: Minecraft = ProxiedConnection.create()
 except ConnectionRefusedError:
-    print("Failed to connect to Minecraft. Using dummy connection.")
-
-    class Minecraft(DummyConnection):
-        pass
-
-    connection: Minecraft = Minecraft()
+    logging.critical(
+        'Server connection failed. Using dummy connection.'
+    )
+    connection: Minecraft = DummyConnection.create()
