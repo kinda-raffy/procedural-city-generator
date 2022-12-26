0c6522744042483d646623ad508c2bf936ad3540
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-27 03:55:28 +1100

    Add math extensions to mcpi.vec3

---
+++ generation/structure/utils/math_vec3.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from mcpi.vec3 import Vec3
+
+
+class MathVec3(Vec3):
+    """Extends mcpi.vec3 with mathematical dunders."""
+    def __truediv__(self, other) -> MathVec3:
+        return MathVec3(self.x / other, self.y / other, self.z / other)
+
+    def __itruediv__(self, other) -> MathVec3:
+        return MathVec3(self / other)
+
+    def __floordiv__(self, other) -> MathVec3:
+        return MathVec3(self.x // other, self.y // other, self.z // other)
+
+    def __abs__(self) -> MathVec3:
+        return MathVec3(abs(self.x), abs(self.y), abs(self.z))
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y, self.z))
+
+    def __mod__(self, other) -> MathVec3:
+        return MathVec3(self.x % other, self.y % other, self.z % other)
