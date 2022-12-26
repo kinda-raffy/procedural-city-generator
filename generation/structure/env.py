d4638b83d3d7764ae258e0617844300abf90b1da
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-27 04:04:17 +1100

    Fix Biome tuple key error and specify types.

--- generation/structure/env.py
+++ generation/structure/env.py
@@ -1,23 +1,28 @@
-from generation.structure.errors.structure import BiomeNotImplemented
+from __future__ import annotations
 from typing import (
-    Dict, Any, Final,
+    Dict,
+    Any,
+    Final,
     NotRequired,
     TypedDict,
-    Type
+    Type,
+    TYPE_CHECKING,
 )
-from utils.block_extension import BlockExt as BlocEx
-from generation import connection as server_conn
-from builder import (
-    Builder,
-    HouseBuilder,
-    ApartmentBuilder,
-    SkyscraperBuilder
-)
-from generation.biome import Biome
 from dataclasses import dataclass
-from mcpi.vec3 import Vec3
 import random
 import tomllib
+
+from mcpi.vec3 import Vec3
+from generation import connection as server_conn
+from generation.structure.errors.structure \
+    import BiomeNotImplemented
+from generation.structure.utils.block_extension \
+    import BlockExt as BlocEx
+from generation.biome import Biome
+
+if TYPE_CHECKING:
+    from builder import Builder
+
 
 __all__ = [
     "MaterialPack",
@@ -65,13 +70,13 @@ class Environment:
 
     def __new__(cls, biome, builder):
         if biome in (Biome.LAVA, Biome.JUNGLE):
-            # TODO: Implement these biomes.
+            # TODO: Implement biomes.
             raise BiomeNotImplemented(biome)
         return super().__new__(cls)
 
     def get_material_pack(self) -> MaterialPack:
         """Load materials from TOML file."""
-        biome: Final[str] = self.biome.name.lower()
+        biome: Final[str] = self.biome[0].name.lower()
         structure_type: Final[str] = \
             self.builder.__name__.removesuffix('Builder').lower()
 
@@ -104,4 +109,4 @@ class Environment:
             block_dimensions + Vec3(0, ground, 0),
             block_dimensions,
             BlocEx['AIR'],
-        )
+            )
