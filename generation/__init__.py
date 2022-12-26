0cd1d66dd76acb3a611cbe4dfdba9aeeba754b75
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-27 03:49:06 +1100

    Fix method overriding in DummyConnection.

--- generation/__init__.py
+++ generation/__init__.py
@@ -4,14 +4,19 @@ import logging
 import inspect
 
 
-class DummyConnection(Minecraft):
+class ServerConnection:
+    """Type-safe wrapper for a Minecraft connection."""
     def postToChat(self, msg) -> NoReturn: ...
     def setBlock(self, *args) -> NoReturn: ...
     def setBlocks(self, *args) -> NoReturn: ...
     def getBlock(self, *args) -> int: ...
 
 
-class ProxiedConnection(Minecraft):
+class DummyConnection(ServerConnection):
+    """Dummy connection for testing."""
+
+
+class ProxiedConnection(Minecraft, ServerConnection):
     def postToChat(self, msg: str) -> NoReturn:
         caller: Final[int] = 1
         module = inspect.getmodule(inspect.stack()[caller][0])
@@ -20,9 +25,9 @@ class ProxiedConnection(Minecraft):
 
 
 try:
-    connection: Minecraft = ProxiedConnection.create()
+    connection: ServerConnection = ProxiedConnection.create()
 except ConnectionRefusedError:
     logging.critical(
         'Server connection failed. Using dummy connection.'
     )
-    connection: Minecraft = DummyConnection.create()
+    connection: ServerConnection = DummyConnection()
