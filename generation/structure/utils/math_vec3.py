3f0bf436c6a40b81bc17a850f46b8222d7afcf8a
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-12-27 16:41:44 +1100

    Specify other and ret types

--- generation/structure/utils/math_vec3.py
+++ generation/structure/utils/math_vec3.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
+from typing import overload
 from mcpi.vec3 import Vec3
 
 
 class MathVec3(Vec3):
     """Extends mcpi.vec3 with mathematical dunders."""
-    def __truediv__(self, other) -> MathVec3:
+    def __truediv__(self, other: MathVec3) -> MathVec3:
         return MathVec3(self.x / other, self.y / other, self.z / other)
 
-    def __itruediv__(self, other) -> MathVec3:
+    def __itruediv__(self, other: MathVec3) -> MathVec3:
         return MathVec3(self / other)
 
-    def __floordiv__(self, other) -> MathVec3:
+    def __floordiv__(self, other: MathVec3) -> MathVec3:
         return MathVec3(self.x // other, self.y // other, self.z // other)
 
     def __abs__(self) -> MathVec3:
@@ -19,5 +20,5 @@ class MathVec3(Vec3):
     def __hash__(self) -> int:
         return hash((self.x, self.y, self.z))
 
-    def __mod__(self, other) -> MathVec3:
+    def __mod__(self, other: MathVec3) -> MathVec3:
         return MathVec3(self.x % other, self.y % other, self.z % other)
